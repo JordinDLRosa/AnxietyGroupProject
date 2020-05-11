7ae249418535bafb186bd38ce02d4e593c3cc062
Hiulingyen <hiulingyen@gmail.com>
2020-05-11 16:42:52 -0400

    A new Scene from Hiuling
    Eyes from others

---
+++ AnxietyGroupProject/Assets/Eyess/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+  void OnMouseEnter(){
+  Destroy(gameObject);
+}
+}
