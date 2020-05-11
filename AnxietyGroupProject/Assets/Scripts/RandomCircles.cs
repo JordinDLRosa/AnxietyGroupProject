0549596f343f96a40bffe7c995dbface393457cf
Jordin De La Rosa <jordindlrosa@gmail.com>
2020-05-11 10:02:12 -0400

    Explaining what we will do

--- AnxietyGroupProject/Assets/Scripts/RandomCircles.cs
+++ AnxietyGroupProject/Assets/Scripts/RandomCircles.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class RandomCircles : MonoBehaviour
 {
 
-    public GameObject EveryonePrefab;
+    public GameObject[] EveryonePrefab;
     public float spawnWait;
     public int startWait;
     public bool stop;
 
+    
     public int circleSpawned = 0;
     public int maxCircles;
 
@@ -42,7 +43,7 @@ public class RandomCircles : MonoBehaviour
         { 
             Vector3 spawnPos = new Vector3(Random.Range(0f, 1000f), Random.Range(0f, 320f), 1);
 
-            Instantiate(EveryonePrefab, spawnPos + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            Instantiate(EveryonePrefab[randCircle], spawnPos + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
 
             yield return new WaitForSeconds(spawnWait);
         }
