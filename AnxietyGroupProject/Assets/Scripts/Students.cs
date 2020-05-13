14cceb381948f472e00e859e7ff735756883bc82
Jordin De La Rosa <jordindlrosa@gmail.com>
2020-05-13 13:36:52 -0400

    Scenario in Progress

---
+++ AnxietyGroupProject/Assets/Scripts/Students.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Students : MonoBehaviour
+{
+    public Transform Classmates;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for ( int i = 0; i < 10; i++)
+        {
+            Instantiate(Classmates, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
