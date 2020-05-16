1d64f0b8240808a9add439e5af8dfaa46973b4ad
Unknown <odalys.sanchez@mail.citytech.cuny.edu>
2020-05-16 15:45:21 -0400

    Odalys's scene

---
+++ AnxietyGroupProject/Assets/Scripts/TextScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+  
+    }
+
+public void OnCollisionEnter2D(Collision2D col)
+{
+     if (col.gameObject.tag == "Positive")
+     {
+        var TextScript = col.gameObject.GetComponent<TextScript>();
+        
+      }
+
+   }
+}
