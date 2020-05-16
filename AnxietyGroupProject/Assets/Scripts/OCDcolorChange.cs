6151616dcbc5e4a3db6a1a2d0c5c141f158d210f
Bryan P Louis <bryan.pierrelouis@mail.citytech.cuny.edu>
2020-05-16 19:01:15 -0400

    AlmostTherebutNotQuite

---
+++ AnxietyGroupProject/Assets/Scripts/OCDcolorChange.cs
@@ -0,0 +1,23 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OCDcolorChange : MonoBehaviour
+{
+
+	public Material[] material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
---
+++ AnxietyGroupProject/Assets/Scripts/changeText.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeText : MonoBehaviour
+{
+	public string playerDialogOption1;
+	public string playerDialogOption2;
+	public string playerDialogOption3;
+	public string playerDialogOption4;
+
+	private int numberOfTimesIHitSpaceBar = 0;
+	public Text playerTextBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        	numberOfTimesIHitSpaceBar++;
+
+        	if(numberOfTimesIHitSpaceBar ==1)
+        	{
+        		playerTextBox.text = playerDialogOption1;
+        	}
+        	else if(numberOfTimesIHitSpaceBar == 2)
+        	{
+        		playerTextBox.text = playerDialogOption2;
+        	}
+            else if(numberOfTimesIHitSpaceBar == 3)
+            {
+                playerTextBox.text = playerDialogOption3;
+            }
+            else if(numberOfTimesIHitSpaceBar == 4)
+            {
+                playerTextBox.text = playerDialogOption4;
+            }
+
+        }
+    }
+}
