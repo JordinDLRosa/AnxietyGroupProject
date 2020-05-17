90705e6d9c691318a1cd4965d67eea6e95823429
Odalys <odalys.sanchez@mail.citytech.cuny.edu>
2020-05-17 13:52:55 -0400

    update on my scene

--- AnxietyGroupProject/Assets/Scripts/GameManager.cs
+++ AnxietyGroupProject/Assets/Scripts/GameManager.cs
@@ -3,35 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
-    
-    public GameObject Negative;
-    public GameObject Positive;
-    //public GameObject square;
+    GameObject Text1Prefab;
+    GameObject Positive1Prefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Text1Prefab = Resources.Load<GameObject>("Text1") as GameObject;
+        Positive1Prefab = Resources.Load<GameObject>("Positive1") as GameObject;
 
+    }
     // Update is called once per frame
     void Update()
     {
-        //timer = t++;
-
-        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        //Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
         Vector2 pos = new Vector2(Random.Range(-7.0f, 7.0f), Random.Range(-7.0f, 6.0f));
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(Negative, pos, Quaternion.identity);
+            Instantiate(Text1Prefab, pos, Quaternion.identity);
         }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Destroy(Text1Prefab);
+        //    Instantiate(Positive1Prefab, worldPosition, Quaternion.identity);
+        //}
 
-        else if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(Positive, worldPosition, Quaternion.identity);
-      
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log("Something was clicked!");
+                Debug.Log(hit.collider.gameObject.name);
+                GameObject.Destroy(hit.collider.gameObject);
+                Instantiate(Positive1Prefab, mousePos2D, Quaternion.identity);
+            }
+
         }
     }
 }
